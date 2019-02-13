d6479b7eb2edd38e2eb4b4eea8f9c3603defe701
Romavic <romavicdosanjoskc@gmail.com>
2019-02-13 18:09:20 +0200

    13&02&2019 I to beginner create this project....

---
+++ app/src/main/java/com/autonacao/dicionariodetermosinformticos/Activity_Dictionary_Words.java
@@ -0,0 +1,13 @@
+package com.autonacao.dicionariodetermosinformticos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Dictionary_Words extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dictionary_words);
+    }
+}
---
+++ app/src/main/java/com/autonacao/dicionariodetermosinformticos/Activity_Splash_Screen.java
@@ -0,0 +1,14 @@
+package com.autonacao.dicionariodetermosinformticos;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Activity_Splash_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+    }
+}
